c55c1fb6969135299d0c0ba7e0c9322385a6cba5
Haakio <matheolecoco9@gmail.com>
2025-04-30 19:08:41 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,13 +1,8 @@
-export function changeDimension(dimension, dimensions, callback) {
-    if (dimensions[dimension]) {
-        currentDimension = dimension;
-        document.body.style.background = dimensions[dimension].background;
-        document.body.style.color = dimensions[dimension].color;
-        clickMultiplier *= dimensions[dimension].boost;
-        document.body.classList.add('fade-transition');
-        setTimeout(() => {
-            document.body.classList.remove('fade-transition');
-        }, 1000);
-        callback();
-    }
+// Fonctions utilitaires pour le jeu
+function formatNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
